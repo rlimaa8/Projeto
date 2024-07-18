6332366511ca3b32f1a57e96bf0015a81dd161c7
rlimaa8 <116236692+rlimaa8@users.noreply.github.com>
2024-07-18 12:30:03 -0300

    avatar change

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
-  }
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "./assets/avatar-light.png")
+  // } else {
+  //   img.setAttribute("src", "./assets/avatar.png")
+  // }
 }
