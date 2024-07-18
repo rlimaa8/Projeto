82d3c4a5f2afc8ff1b929e4dcad14ebc3420b56b
rlimaa8 <116236692+rlimaa8@users.noreply.github.com>
2024-07-18 12:46:14 -0300

    add readme

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  const img = document.querySelector("#profile img")
 
+  //const img = document.querySelector("#profile img")
   // if (html.classList.contains("light")) {
   //   img.setAttribute("src", "./assets/avatar-light.png")
   // } else {
